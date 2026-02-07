d5379cfb44bb8efab896b6509a2f9dd37658c7f9
NGamaniprasad <134373459+NGamaniprasad@users.noreply.github.com>
2026-02-07 13:50:17 +0530

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -2,7 +2,29 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 SHEET_ID = ""
+"""
+Where to find Google Cloud service account key
 
+This is found in Google Cloud Console, not in Sheets.
+
+Steps:
+
+Go to: https://console.cloud.google.com
+
+Select your project (top left)
+
+IAM & Admin → Service Accounts
+
+Click your service account
+
+Go to Keys tab
+
+Add Key → Create new key → JSON
+
+It will download a file to your computer.
+
+That file is the key.  edir direct-sale.json 
+"""
 def save_to_sheet(row):
     scope = [
         "https://spreadsheets.google.com/feeds",
